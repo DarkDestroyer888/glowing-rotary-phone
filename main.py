f0c156483dddb8b796073505b2d2c03037fdb4a9
David Shim <davidshim0331@gmail.com>
2020-12-20 22:51:28 +0000

    initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+import discord
+import os
+
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged in as')
+        print(self.user.name)
+        print(self.user.id)
+        print('------')
+
+    async def on_message(self, message):
+        # we do not want the bot to reply to itself
+        if message.author.id == self.user.id:
+            return
+
+        if message.content.startswith('!hello'):
+            await message.channel.send('Hello!')
+        if message.content.lower() == 'josh is baf':
+            await message.channel.send('I agree')
+
+
+client = MyClient()
+client.run(os.environ['DISCORD_TOKEN'])
